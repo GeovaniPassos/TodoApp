80a041af18d934d6d6ce799e2138780ae5655961
Geovani dos Passos <84425834+GeovaniPassos@users.noreply.github.com>
2023-01-20 16:19:22 -0300

    Aula 11j
    Exercicio criar o controlador de projetos

--- src/main/java/controler/ProjectControler.java
+++ src/main/java/controler/ProjectControler.java
@@ -5,10 +5,50 @@
  */
 package controler;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+import model.Project;
+import util.ConnectionFactory;
+
 /**
  *
  * @author Geovani-PC
  */
 public class ProjectControler {
     
+    public void save(Project project){
+        
+        String sql = "INSERT INTO projects(name,"
+                + " description, "
+                + "createAt, "
+                + "updateAt) VALUE(?,?,?,?)";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            
+            statement.setString(1, project.getName());
+            
+            
+            
+        } catch (Exception e) {
+        }
+    }
+    
+    public void update(Project project){
+        
+    }
+    
+    public void removeById(int Id){
+        
+    }
+    
+    public List<Project> getAll(int id){
+       
+        return null;
+    }
 }
