4f903826fddbb6547b4a9b659de384df0d72f749
Geovani dos Passos <84425834+GeovaniPassos@users.noreply.github.com>
2023-02-02 13:40:38 -0300

    Aula 16 C
    Implementando o Model da Lista de Tarefas

---
+++ src/main/java/util/TaskTableModel.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import model.Task;
+
+/**
+ *
+ * @author Geovani-PC
+ */
+public class TaskTableModel extends AbstractTableModel{
+    
+    String[] columns = {"Nome", "Descrição", "Prazo", "Tarefa concluída", "Editar", "Excluir"};
+    List<Task> tasks = new ArrayList();
+
+    @Override
+    public int getRowCount() {
+        return tasks.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columns.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        
+        switch(columnIndex){
+            case 1: 
+                return tasks.get(rowIndex).getName();
+                
+                
+            case 2:
+                return tasks.get(rowIndex).getDescription();
+                
+                
+            case 3:
+                return tasks.get(rowIndex).getDeadline();
+                
+            case 4:
+                return tasks.get(rowIndex).isIsCompleted();
+                
+                
+            case 5:
+                return "";
+
+            case 6:
+                return "";
+                
+                
+            default:
+        }
+    }
+    
+}
