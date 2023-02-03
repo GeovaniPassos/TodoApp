0d1c93c946a5c187a61513f333676bc22ef51f20
Geovani dos Passos <84425834+GeovaniPassos@users.noreply.github.com>
2023-02-03 10:41:14 -0300

    Aula 16H
    Passagem de parâmetro para retornar oque a coluna é, return value

--- src/main/java/util/TaskTableModel.java
+++ src/main/java/util/TaskTableModel.java
@@ -37,10 +37,18 @@ public class TaskTableModel extends AbstractTableModel{
     }
     
     public boolean isCellEditable(int rowIndex, int columnIndex){
-        if (columnIndex == 3)
-            return true;
-        else 
-            return false;
+        return columnIndex == 3;
+    
+    }
+    
+    @Override
+    public Class<?> getColumnClass(int columnIndex){
+        
+        if (tasks.isEmpty()){
+            return Object.class;
+        }
+        return this.getValueAt(0, columnIndex).getClass(); 
+        
     }
 
     @Override
